b54160155da7ee70b9667000db106005496e998c
michal92299 <chudybyczek2137@gmail.com>
2026-06-04 20:10:54 +0200

    Update main.rs

--- source-code/gui/src-tauri/src/main.rs
+++ source-code/gui/src-tauri/src/main.rs
@@ -1,8 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
-    tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_http::init())
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    hexai_gui_lib::run()
 }
